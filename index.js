5625403eafd13294d5722d6485f7700526d68e87
Kellymugambi <kellymgambi@.com>
2022-11-26 23:07:33 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter (
+        function (max){
+            if (max.toUpperCase()===name.toUpperCase()) {
+                return true;
+            }
+        }
+    )
+}
+
+function fuzzyMatch (drivers, name){
+    return drivers.filter (
+        function (max){
+            if (max.slice(0,name.length)===name) {
+                return true;
+            }
+        }
+    )
+}
+   
+function  matchName (drivers, name){
+    return drivers.filter (
+        function (max){
+            if (max.name===name) {
+                return true;
+            }
+        }
+    )
+}
